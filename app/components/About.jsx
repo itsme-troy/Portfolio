aa8d5e48858682b0693b8dd97c323809fac9466d
Leobert Troy Teodoro <teodoroleoberttroy19@gmail.com>
2025-08-23 01:22:05 +0800

    added motion in about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,64 +1,117 @@
 import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
-
+import {motion} from "motion/react" 
 const About = () => {
   return (
-    <div id='about' className='w-full px-[12%] py-10
-        scroll-mt-20' >
-      <h4 className='text-center mb-2 text-lg font-Ovo'> Introduction </h4>
-      <h2 className='text-center text-5xl font-Ovo'>
-         About me </h2>
+    <motion.div id='about' className='w-full px-[12%] py-10 scroll-mt-20' 
+        initial={{ opacity: 0}}
+        whileInView={{ opacity: 1}} // end on orig size
+        transition={{duration: 1}} 
+   >
+      <motion.h4 
+        initial={{ opacity: 0, y: -20}}
+        whileInView={{ opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.3}} 
+      
+        className='text-center mb-2 text-lg font-Ovo'> Introduction </motion.h4>
+      
+      <motion.h2 
+        initial={{ opacity: 0, y: -20}}
+        whileInView={{ opacity: 1, y: 0}}
+        transition={{duration: 0.5, delay: 0.5}} 
+      
+        className='text-center text-5xl font-Ovo'>
+         About me </motion.h2>
 
-        <div className='flex w-full flex-col lg:flex-row
+        <motion.div
+            initial={{ opacity: 0}}
+            whileInView={{ opacity: 1}}
+            transition={{duration: 0.5, delay: 0.8}} 
+            
+            className='flex w-full flex-col lg:flex-row
              items-center gap-20 my-20'>
-            <div className='w-64 sm:w-80 rounded-3x1 max-w-none'>
+            <motion.div className='w-64 sm:w-80 rounded-3x1 max-w-none'
+                initial={{ opacity: 0, scale: 0.9}}
+                whileInView={{ opacity: 1, scale:1}}
+                transition={{duration: 0.6}} 
+                
+                >
+                
                 <Image src={assets.user_image} alt='user'
                     className='w-full rounded-3xl'/>
-            </div>
+            </motion.div>
 
-            <div className='flex-1'>
-          <p className="mb-10 max-w-2xl font-Ovo">
-            I am a passionate software developer with hands-on experience from academic projects and internships. 
-            My expertise lies in building responsive, user-friendly web applications using modern technologies such as 
-            <span className="font-semibold"> Python, Django, React, Next.js, Tailwind CSS,</span> and <span className="font-semibold">Ruby on Rails</span>. 
-          </p>
-          <p className="mb-10 max-w-2xl font-Ovoo">
-            I thrive in collaborative environments, continuously learning new tools and best practices to deliver high-quality work. 
-            Beyond coding, I enjoy solving real-world problems, contributing to team success, and creating solutions that make a positive impact. </p>
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1}}
+                transition={{ duration: 0.6, delay: 0.8}} 
+            
+               className='flex-1'>
+                <p className="mb-10 max-w-2xl font-Ovo">
+                    I am a passionate software developer with hands-on experience from academic projects and internships. 
+                    My expertise lies in building responsive, user-friendly web applications using modern technologies such as 
+                    <span className="font-semibold"> Python, Django, React, Next.js, Tailwind CSS,</span> and <span className="font-semibold">Ruby on Rails</span>. 
+                </p>
 
-                <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 
-                    max-w-2xl'>
-                    {infoList.map(({icon, iconDark, title, description}, 
-                        index) => (
-                            <li className='border-[0.5px] border-gray-400 rounded-xl
-                                p-6 cursor-pointer hover:bg-lightHover 
-                                hover:-translate-y-1 duration-500'
+                <p className="mb-10 max-w-2xl font-Ovoo">
+                    I thrive in collaborative environments, continuously learning new tools and best practices to deliver high-quality work. 
+                    Beyond coding, I enjoy solving real-world problems, contributing to team success, and creating solutions that make a positive impact. </p>
+
+                        <motion.ul 
+                            initial={{ opacity: 0}}
+                            whileInView={{ opacity: 1}}
+                            transition={{duration: 0.8, delay: 1}} 
+                        
+                            className='grid grid-cols-1 sm:grid-cols-3 gap-6 
+                            max-w-2xl'>
+                            {infoList.map(({icon, iconDark, title, description}, 
+                                index) => (
+                                    <motion.li 
+                                        whileInView={{ scale: 1.05}}
+                                    
+                                    className='border-[0.5px] border-gray-400 rounded-xl
+                                        p-6 cursor-pointer hover:bg-lightHover 
+                                        hover:-translate-y-1 duration-500'
+                                        key={index}>
+                                        <Image src={icon} alt={title} className='w-7 mt-3' />
+                                        <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                                        <p className='text-gray-600 text-sm'>{description}</p>
+                                </motion.li>
+                            ))}
+                        </motion.ul>
+
+                        <motion.h4
+                            initial={{ y:20, opacity: 0}}
+                            whileInView={{ y:0, opacity: 1}}
+                            transition={{delay: 1.3, delay: 0.5}} 
+                        
+                        className='my-6 text-gray-700 font-Ovo'> Tools i use </motion.h4>
+                        <motion.ul  
+                            initial={{ opacity: 0}}
+                            whileInView={{ opacity: 1}}
+                            transition={{delay: 1.5, delay: 0.6}} 
+                        
+                        
+                        className='flex items-center gap-3 sm:gap-5'>
+                            {toolsData.map((tool, index)=>(
+                                <motion.li 
+                                
+                                
+                                    className='flex items-center justify-center w-12 sm:w-14 
+                                    aspect-square border border-gray-400 rounded-lg cursor-pointer
+                                    hover:-translate-y-1 duration-500' 
                                 key={index}>
-                                <Image src={icon} alt={title} className='w-7 mt-3' />
-                                <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
-                                <p className='text-gray-600 text-sm'>{description}</p>
-                        </li>
-                    ))}
-                </ul>
-
-                <h4 className='my-6 text-gray-700 font-Ovo'> Tools i use </h4>
-                <ul className='flex items-center gap-3 sm:gap-5'>
-                    {toolsData.map((tool, index)=>(
-                        <li className='flex items-center justify-center w-12 sm:w-14 
-                            aspect-square border border-gray-400 rounded-lg cursor-pointer
-                            hover:-translate-y-1 duration-500' 
-                        key={index}>
-                            <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
-                        </li>
-                    ))}
-                </ul>
+                                    <Image src={tool} alt='Tool' className='w-5 sm:w-7'/>
+                                </motion.li>
+                            ))}
+                        </motion.ul>
 
 
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
 
-    </div>
+    </motion.div>
   )
 }
 
